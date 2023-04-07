48c557d96137a72463831d8590ce50585341bc56
Charles Eduardo <charleseduardofaria@gmail.com>
2023-04-07 09:10:06 -0300

    Dividindo as funcionalidades de transações em diferentes arquivos

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import './models/transaction.dart';
+import './components/transaction_list.dart';
+import './components/transaction_form.dart';
+
 void main() => runApp(ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
@@ -36,7 +38,6 @@ class MyHomePage extends StatelessWidget {
         title: Text('Expenses'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
@@ -46,55 +47,8 @@ class MyHomePage extends StatelessWidget {
               color: Colors.blue,
             ),
           ),
-          Column(
-            children: _transactions.map((tr) {
-              return Card(
-                child: Row(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        ),
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        "R\$ ${tr.value.toStringAsFixed(2)}",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          tr.title,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          )
-                        ),
-                        Text(
-                          DateFormat('d MMM y').format(tr.date),
-                          style: TextStyle(
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              );
-            }).toList(),
-          )
+          TransactionList(_transactions),
+          TransactionForm(),
         ],
       ),
     );
